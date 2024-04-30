9744182aa111d4190ef5571afec7e317afa6e9ad
Steven Hoang <drunkcoding@outlook.com>
2024-04-30 13:30:07 +0800

    Update Tunnel.ts

--- src/CloudFlare/Tunnel.ts
+++ src/CloudFlare/Tunnel.ts
@@ -1,7 +1,7 @@
-import { DefaultK8sArgs } from '../types';
-import Deployment from '../Deployment';
-import { Input } from '@pulumi/pulumi';
-import { TunnelHelmParameters } from './Tunnel-Helm';
+import { DefaultK8sArgs } from "../types";
+import Deployment from "../Deployment";
+import { Input } from "@pulumi/pulumi";
+import { TunnelHelmParameters } from "./Tunnel-Helm";
 
 export type TunnelParameters = {
   token: Input<string>;
@@ -10,14 +10,14 @@ export type TunnelParameters = {
   tcp?: Array<{ host: string; service: string; port: number }>;
 };
 
-export interface TunnelProps extends Omit<DefaultK8sArgs, 'name'> {
+export interface TunnelProps extends Omit<DefaultK8sArgs, "name"> {
   name?: string;
   replicas?: number;
   parameters: TunnelParameters;
 }
 
 export default ({
-  name = 'tunnel',
+  name = "tunnel",
   namespace,
   parameters,
   replicas = 2,
@@ -26,29 +26,29 @@ export default ({
 }: TunnelProps) => {
   const tcpArgs = parameters.tcp
     ? parameters.tcp.flatMap((t) => [
-        '--hostname',
+        "--hostname",
         t.host,
-        '--url',
+        "--url",
         `tcp://${t.service}:${t.port}`,
       ])
     : [];
 
   const args = [
-    'tunnel',
+    "tunnel",
     ...tcpArgs,
-    '--no-autoupdate',
-    'run',
-    '--token',
-    '$(token)',
+    "--no-autoupdate",
+    "run",
+    "--token",
+    "$(token)",
     // '--config',
     // '/etc/cloudflared/config/config.yaml',
   ];
 
   if (parameters.enableMetrics) {
-    args.push('--metrics', '0.0.0.0:8081');
+    args.push("--metrics", "0.0.0.0:8081");
   }
 
-  const tunnel = Deployment({
+  return Deployment({
     name,
     namespace,
 
@@ -56,12 +56,27 @@ export default ({
 
     podConfig: {
       ports: { http: 3000 },
-      image: 'cloudflare/cloudflared:latest',
-      podSecurityContext: { readOnlyRootFilesystem: true },
+      image: "cloudflare/cloudflared:latest",
+      securityContext: {
+        fsGroup: 10000,
+        runAsUser: 10000,
+        runAsGroup: 10000,
+        sysctls: [
+          {
+            name: "net.ipv4.ping_group_range",
+            value: "10000",
+          },
+        ],
+      },
+      podSecurityContext: {
+        readOnlyRootFilesystem: true,
+        allowPrivilegeEscalation: false,
+        runAsNonRoot: true,
+      },
       probes: {
         liveness: parameters.enableLiveness
           ? {
-              httpGet: '/ready',
+              httpGet: "/ready",
               port: 8081,
               initialDelaySeconds: 10,
               periodSeconds: 10,
@@ -78,6 +93,4 @@ export default ({
 
     ...others,
   });
-
-  return tunnel;
 };
